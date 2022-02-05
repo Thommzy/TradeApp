33be2613162c52ddd46ff9046f89b1c7da7adaef
thommzy <tomiwaobeisun@yahoo.com>
2022-02-05 20:11:05 +0100

    StatModel

--- VarensTradeApp/VarensTradeApp/Model/StatModel.swift
+++ VarensTradeApp/VarensTradeApp/Model/StatModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+enum Section {case first}
+
+import Foundation
+import UIKit
+
+struct StatModel: Codable {
+    var uuid = UUID()
+    let icon: String
+    let title: String
+    let price: String
+}
+
+extension StatModel: Hashable {
+    static func ==(lhs: StatModel, rhs: StatModel) -> Bool {
+        return lhs.uuid == rhs.uuid
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(uuid)
+    }
+}
